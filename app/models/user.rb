4ba00c705863371aac18388e90e962a07c66b031
Maciek Lenc <maciej.lenc@sage.com>
2015-04-23 10:54:51 +0100

    More validation & secure password for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
+  before_save { email.downcase! }
+
   has_many :microposts
-  validates :name, presence: true
-  validates :email, presence: true
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255},
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  validates :password, length: { minimum: 6 }
 end
