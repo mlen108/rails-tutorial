018b491d92c05a751997717ba1e27c8d7d99b95c
Maciek Lenc <maciej.lenc@sage.com>
2015-04-24 15:50:32 +0100

    Tests wrong placed

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -104,27 +104,4 @@ class UsersControllerTest < ActionController::TestCase
     end
     assert_redirected_to root_url
   end
-
-  test "index as admin including pagination and delete link" do
-    log_in_as(@user)
-    get users_path
-    assert_template 'users/index'
-    assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
-    first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @user
-        assert_select 'a[href=?]', user_path(user), text: 'delete', method: :delete
-      end
-    end
-    assert_difference 'User.count', -1 do
-      delete user_path(@other_user)
-    end
-  end
-
-  test "index as non-admin" do
-    log_in_as(@user)
-    get users_path
-    assert_select 'a', text: 'delete', count: 0
-  end
 end
