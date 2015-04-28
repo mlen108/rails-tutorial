83204a9b68c0a8060de91a6f4e3a9f7a73f6ff6c
Maciek Lenc <maciej.lenc@sage.com>
2015-04-28 13:10:54 +0100

    Probably better to remove the old Micropost things

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,6 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-
-  resources :microposts
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
